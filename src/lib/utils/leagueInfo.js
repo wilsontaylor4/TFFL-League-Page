5d22e42d1d0a8289e238295767eca3d2a57a61d8
wilsontaylor4 <wilsontaylor4@yahoo.com>
2024-10-27 18:12:52 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -23,7 +23,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 export const managers = [
     {
       "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-      "managerID" : "wilsontaylor4",
+      "managerID" : "78956523485020160",
       "name": "Taylor Wilson",
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Vancouver, WA", // (optional)
